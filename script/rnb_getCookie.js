237158d6229d91be9ff53a51a6dcc6fbebed9eb5
zhaoguibin <940508445@qq.com>
2020-07-18 21:02:08 +0800

    rnbmix getcookie

---
+++ script/rnb_getCookie.js
@@ -0,0 +1,4 @@
+/*************
+获取mixrnb的cookie = type=http-request,pattern=^http:\/\/www\.mixrnb\.com\/space-uid-\d*\.html script-path=http-request.js,max-size=16384,debug=true
+**************/
+$notification.post('','','匹配url测试');
