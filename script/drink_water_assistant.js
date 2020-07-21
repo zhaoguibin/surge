b88cf7cf2c573719f494c6831233c330855f55d6
zhaoguibin <940508445@qq.com>
2020-07-21 11:55:08 +0800

    2321313

--- script/drink_water_assistant.js
+++ script/drink_water_assistant.js
@@ -1,6 +1,6 @@
 /*************************
  [Script]
- 喝水小助手.js = type=cron,cronexp=* * 9-17 * * 1-5,wake-system=1,timeout=20,script-path=https://raw.githubusercontent.com/zhaoguibin/surge/master/script/RNB_daily_bonus.js
+ 喝水小助手.js = type=cron,cronexp=* * 9-17 * * 1-5,wake-system=1,timeout=20,script-path=https://raw.githubusercontent.com/zhaoguibin/surge/master/script/drink_water_assistant.js
  *************************/
 
 var Date = new Date();
