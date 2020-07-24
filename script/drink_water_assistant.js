23a36dc33b21427731afd5e4c325a74ab72ac755
zhaoguibin <940508445@qq.com>
2020-07-24 11:48:59 +0800

    2132133

--- script/drink_water_assistant.js
+++ script/drink_water_assistant.js
@@ -8,14 +8,15 @@ var time = new Array();
 time[9] = 1;
 time[10] = 2;
 time[11] = 3;
-time[12] = 4;
-time[13] = 5;
-time[14] = 6;
-time[15] = 7;
-time[16] = 8;
-time[17] = 9;
+time[13] = 4;
+time[14] = 5;
+time[15] = 6;
+time[16] = 7;
+time[17] = 8;
 
-var str = "这是今天第" + time[myDate.getHours()] + "轮提醒";
+if(time[myDate.getHours()]){
+    var str = "这是今天第" + time[myDate.getHours()] + "轮提醒";
+    $notification.post('喝水小助手', myDate.toLocaleString(), str)
+}
 
-$notification.post('喝水小助手', myDate.toLocaleString(), str)
 $done();
