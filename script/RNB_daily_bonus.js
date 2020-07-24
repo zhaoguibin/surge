647eee27d4b2fb025e4a80efdaa4ca2ad68595ea
zhaoguibin <940508445@qq.com>
2020-07-24 10:29:03 +0800

    获取cookie放到同一个脚本

--- script/RNB_daily_bonus.js
+++ script/RNB_daily_bonus.js
@@ -1,22 +1,51 @@
 /*************************
  [Script]
  mixrnb签到.js = type=cron,cronexp=35 8 * * *,wake-system=1,timeout=20,script-path=https://raw.githubusercontent.com/zhaoguibin/surge/master/script/RNB_daily_bonus.js
+
+ 获取mixrnb的cookie = type=http-request,pattern=http:\/\/www\.mixrnb\.com\/space-uid-\d*\.html,script-path=https://raw.githubusercontent.com/zhaoguibin/surge/master/script/RNB_daily_bonus.js,script-update-interval=0
+ 访问个人空间获取到cookie，http://www.mixrnb.com/space-uid-*.html
  *************************/
-
-//cookie
-const R5nb_c8f5_auth = $persistentStore.read('R5nb_c8f5_auth');
-const saltkey = $persistentStore.read('R5nb_c8f5_saltkey');
-
-if (!saltkey) {
-    $gabeX.notify('', '', '读取R5nb_c8f5_saltkey失败，请先访问个人空间【http://www.mixrnb.com/space-uid-*.html】获取');
-}
-
-if (!R5nb_c8f5_auth) {
-    $gabeX.notify('', '', '读取R5nb_c8f5_auth失败，请先访问个人空间【http://www.mixrnb.com/space-uid-*.html】获取');
-}
-
 var error, response, body;
 var set_cookies = new Array();
+
+const isRequest = typeof $request != "undefined"
+if (isRequest) {
+    $cookie = $request.headers.Cookie;
+
+    const saltkey_regex = /(R5nb_c8f5_saltkey=\S*)/gm;
+    const auth_regex = /(R5nb_c8f5_auth=(\S*));/gm;
+
+    const R5nb_c8f5_saltkey = saltkey_regex.exec($cookie);
+    const R5nb_c8f5_auth = auth_regex.exec($cookie);
+
+    if (!R5nb_c8f5_saltkey) {
+        $notification.post('', '', '获取R5nb_c8f5_saltkey失败');
+        $done();
+    }
+
+    if (!R5nb_c8f5_auth) {
+        $notification.post('', '', '获取R5nb_c8f5_auth失败');
+        $done();
+    }
+
+    $persistentStore.write(R5nb_c8f5_saltkey[1], 'R5nb_c8f5_saltkey');
+
+    if (!$persistentStore.read('R5nb_c8f5_saltkey')) {
+        $notification.post('', '', '保存R5nb_c8f5_saltkey失败');
+        $done();
+    }
+
+    $persistentStore.write(R5nb_c8f5_auth[1], 'R5nb_c8f5_auth');
+
+    if (!$persistentStore.read('R5nb_c8f5_auth')) {
+        $notification.post('', '', '保存R5nb_c8f5_auth失败');
+        $done();
+    }
+
+    $notification.post('', '', '获取cookie成功，请禁用此脚本');
+
+    $done({});
+}
 
 // Modified from yichahucha
 function gabeX() {
@@ -52,9 +81,21 @@ function gabeX() {
         read,
         notify
     }
-};
+}
 
 var $gabeX = gabeX();
+
+//cookie
+const R5nb_c8f5_auth = $persistentStore.read('R5nb_c8f5_auth');
+const saltkey = $persistentStore.read('R5nb_c8f5_saltkey');
+
+if (!saltkey) {
+    $gabeX.notify('', '', '读取R5nb_c8f5_saltkey失败，请先访问个人空间【http://www.mixrnb.com/space-uid-*.html】获取');
+}
+
+if (!R5nb_c8f5_auth) {
+    $gabeX.notify('', '', '读取R5nb_c8f5_auth失败，请先访问个人空间【http://www.mixrnb.com/space-uid-*.html】获取');
+}
 
 var options = {
     url: "http://www.mixrnb.com/plugin.php?id=dsu_paulsign:sign",
