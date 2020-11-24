a9f1295e122ac81040c60272d38295f2841392e4
zhaoguibin <940508445@qq.com>
2020-11-24 11:39:16 +0800

    132·213321

--- script/hao4k_daily_bonus.js
+++ script/hao4k_daily_bonus.js
@@ -1,10 +1,10 @@
 /*************
-[Script]
-hao4k签到.js = type=cron,cronexp=35 8 * * *,wake-system=1,timeout=60,script-path=https://raw.githubusercontent.com/zhaoguibin/surge/master/script/hao4k_daily_bonus.js
+ [Script]
+ hao4k签到.js = type=cron,cronexp=35 8 * * *,wake-system=1,timeout=60,script-path=https://raw.githubusercontent.com/zhaoguibin/surge/master/script/hao4k_daily_bonus.js
 
-获取hao4k的cookie = type=http-request,pattern=https:\/\/www.hao4k.cn\/plugin\.php\?id=k_misign:sign,script-path=https://raw.githubusercontent.com/zhaoguibin/surge/master/script/hao4k_daily_bonus.js,script-update-interval=0
-访问签到页面 https://www.hao4k.cn/plugin.php?id=k_misign:sign
-**************/
+ 获取hao4k的cookie = type=http-request,pattern=https:\/\/www.hao4k.cn\/plugin\.php\?id=k_misign:sign,script-path=https://raw.githubusercontent.com/zhaoguibin/surge/master/script/hao4k_daily_bonus.js,script-update-interval=0
+ 访问签到页面 https://www.hao4k.cn/plugin.php?id=k_misign:sign
+ **************/
 
 var error, response, body;
 var set_cookies = new Array();
@@ -80,9 +80,7 @@ var options = {
         'Cookie': Hao4K_cookie,
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36 Edg/83.0.478.64',
     },
-    body: {
-
-    }
+    body: {}
 }
 
 function getFormHash(error, response, body) {
@@ -92,20 +90,17 @@ function getFormHash(error, response, body) {
         $gabeX.notify('', '', '获取formhash失败');
     }
 
-    options.url = 'https://www.hao4k.cn/plugin.php?id=k_misign:sign&operation=qiandao&format=empty&inajax=1&ajaxtarget=JD_sign&formhash='+formhash;
+    options.url = 'https://www.hao4k.cn/plugin.php?id=k_misign:sign&operation=qiandao&format=empty&inajax=1&ajaxtarget=JD_sign&formhash=' + formhash;
 }
 
 function decodeXml(error, response, body) {
-
     //查看是否登录过期或者没登录
-    var message = '签到出错，请查看日志';
-    // if (body.match('今日已签')) {
-    //     message = '您今日已经签到，请明天再来！';
-    // }
+    var message = '签到出错';
+    if (body.length < 75) {
+        message = '签到成功';
+    }
 
-    var str_length = body.length;
-
-    $gabeX.notify('Hao4k签到提醒', str_length, message);
+    $gabeX.notify('Hao4k签到提醒', '', message);
 }
 
 setTimeout(function () {
