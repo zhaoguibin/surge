aa267b25d9e689c81ff9f715d5ff62e391a0bdfc
zhaoguibin <940508445@qq.com>
2021-01-28 11:42:42 +0800

    234342

--- script/wmsxwd.js
+++ script/wmsxwd.js
@@ -29,3 +29,68 @@ if (isRequest) {
     $notification.post('', '', '获取cookie成功，请禁用此脚本');
     $done();
 }
+
+
+// Modified from yichahucha
+function gabeX() {
+    const get = (options, callback) => {
+        $httpClient.get(options, (error, response, body) => {
+            callback(error, response, body)
+        })
+    }
+
+    const post = (options, callback) => {
+        $httpClient.post(options, (error, response, body) => {
+            callback(error, response, body)
+        })
+    }
+
+    const write = (value, key) => {
+        return $persistentStore.write(value, key)
+    }
+
+    const read = (key) => {
+        return $persistentStore.read(key)
+    }
+
+    const notify = (title, subtitle, message) => {
+        $notification.post(title, subtitle, message)
+        $done();
+    }
+
+    return {
+        get,
+        post,
+        write,
+        read,
+        notify
+    }
+}
+
+var $gabeX = gabeX();
+
+//cookie
+const Wmsxwd_cookie = $persistentStore.read('Wmsxwd_cookie');
+
+if (!Wmsxwd_cookie) {
+    $gabeX.notify('', '', '读取Wmsxwd_cookie失败，请先访问【https://wmsxwd-a.men/user】获取');
+}
+
+var options = {
+    url: "https://wmsxwd-a.men/user/checkin",
+    headers: {
+        ':scheme':'https',
+        'Cookie': Wmsxwd_cookie,
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36 Edg/83.0.478.64',
+    },
+    body: {}
+}
+
+$gabeX.post(options, decodeMessage);
+
+function decodeMessage(error, response, body) {
+
+    var obj = eval('(' + body + ')');
+
+    $gabeX.notify('Hao4k签到提醒', '', obj.msg);
+}
