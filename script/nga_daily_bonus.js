490ceff1a3c47e770ec62b5dbf3248f3f77612f8
zhaoguibin <940508445@qq.com>
2021-03-10 10:39:31 +0800

    nga签到任务

--- script/nga_daily_bonus.js
+++ script/nga_daily_bonus.js
@@ -120,15 +120,8 @@ var options = {
     headers: {
         'Host': 'ngabbs.com',
         'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundaryAgjAve4UZAXpjupu',
-        'Origin': 'https://ngabbs.com',
-        'Accept-Encoding': 'gzip, deflate, br',
         'Cookie': NGA_cookie,
-        'Connection': 'keep-alive',
-        'Accept': '*/*',
         'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 NGA_skull/7.1.8',
-        'Referer': 'https://ngabbs.com/misc/mission/mission.php',
-        'Content-Length': '1091',
-        'Accept-Language': 'zh-cn'
     },
     body: "------WebKitFormBoundaryAgjAve4UZAXpjupu\r\nContent-Disposition: form-data; name=\"__lib\"\r\n\r\ncheck_in\r\n------WebKitFormBoundaryAgjAve4UZAXpjupu\r\nContent-Disposition: form-data; name=\"__output\"\r\n\r\n11\r\n------WebKitFormBoundaryAgjAve4UZAXpjupu\r\nContent-Disposition: form-data; name=\"app_id\"\r\n\r\n1001\r\n------WebKitFormBoundaryAgjAve4UZAXpjupu\r\nContent-Disposition: form-data; name=\"__act\"\r\n\r\ncheck_in\r\n------WebKitFormBoundaryAgjAve4UZAXpjupu\r\nContent-Disposition: form-data; name=\"access_uid\"\r\n\r\n" + NGA_access_uid + "\r\n------WebKitFormBoundaryAgjAve4UZAXpjupu\r\nContent-Disposition: form-data; name=\"access_token\"\r\n\r\n" + NGA_access_token + "\r\n"
 }
@@ -149,6 +142,64 @@ function decodeJson(errors, response, body) {
 
     $gabeX.notify('nga签到', '', msg);
     console.log(body);
+
+    dailySilverCoins();
+    dailyNCoins();
+    addUpDailyBonus();
+}
+
+//每日签到得银币
+function dailySilverCoins() {
+    var options = {
+        url: "https://ngabbs.com/nuke.php",
+        headers: {
+            'Host': 'ngabbs.com',
+            'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundaryAgjAve4UZAXpjupu',
+            'Cookie': NGA_cookie,
+            'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 NGA_skull/7.1.8',
+        },
+        body: "------WebKitFormBoundaryAgjAve4UZAXpjupu\r\nContent-Disposition: form-data; name=\"__lib\"\r\n\r\nmission\r\n------WebKitFormBoundaryAgjAve4UZAXpjupu\r\nContent-Disposition: form-data; name=\"__output\"\r\n\r\n11\r\n------WebKitFormBoundaryAgjAve4UZAXpjupu\r\nContent-Disposition: form-data; name=\"app_id\"\r\n\r\n1001\r\n------WebKitFormBoundaryAgjAve4UZAXpjupu\r\nContent-Disposition: form-data; name=\"__act\"\r\n\r\ncheckin_count_add\r\n------WebKitFormBoundaryAgjAve4UZAXpjupu\r\nContent-Disposition: form-data; name=\"access_uid\"\r\n\r\n" + NGA_access_uid + "\r\n------WebKitFormBoundaryAgjAve4UZAXpjupu\r\nContent-Disposition: form-data; name=\"access_token\"\r\n\r\n" + NGA_access_token + "\r\n------WebKitFormBoundaryAgjAve4UZAXpjupu\r\nContent-Disposition: form-data; name=\"no_compatible_fix\"\r\n\r\n1\r\n------WebKitFormBoundaryAgjAve4UZAXpjupu\r\nContent-Disposition: form-data; name=\"mid\"\r\n\r\n2\r\n"
+    }
+
+    $gabeX.post(options, function (errors, response, body) {
+        console.log(JSON.parse(body))
+    });
+}
+
+//每日签到得N币
+function dailyNCoins() {
+    var options = {
+        url: "https://ngabbs.com/nuke.php",
+        headers: {
+            'Host': 'ngabbs.com',
+            'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundaryAgjAve4UZAXpjupu',
+            'Cookie': NGA_cookie,
+            'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 NGA_skull/7.1.8',
+        },
+        body: "------WebKitFormBoundaryAgjAve4UZAXpjupu\r\nContent-Disposition: form-data; name=\"__lib\"\r\n\r\nmission\r\n------WebKitFormBoundaryAgjAve4UZAXpjupu\r\nContent-Disposition: form-data; name=\"__output\"\r\n\r\n11\r\n------WebKitFormBoundaryAgjAve4UZAXpjupu\r\nContent-Disposition: form-data; name=\"app_id\"\r\n\r\n1001\r\n------WebKitFormBoundaryAgjAve4UZAXpjupu\r\nContent-Disposition: form-data; name=\"__act\"\r\n\r\ncheckin_count_add\r\n------WebKitFormBoundaryAgjAve4UZAXpjupu\r\nContent-Disposition: form-data; name=\"access_uid\"\r\n\r\n" + NGA_access_uid + "\r\n------WebKitFormBoundaryAgjAve4UZAXpjupu\r\nContent-Disposition: form-data; name=\"access_token\"\r\n\r\n" + NGA_access_token + "\r\n------WebKitFormBoundaryAgjAve4UZAXpjupu\r\nContent-Disposition: form-data; name=\"no_compatible_fix\"\r\n\r\n1\r\n------WebKitFormBoundaryAgjAve4UZAXpjupu\r\nContent-Disposition: form-data; name=\"mid\"\r\n\r\n30\r\n"
+    }
+
+    $gabeX.post(options, function (errors, response, body) {
+        console.log(JSON.parse(body))
+    });
+}
+
+//累计签到365天
+function addUpDailyBonus() {
+    var options = {
+        url: "https://ngabbs.com/nuke.php",
+        headers: {
+            'Host': 'ngabbs.com',
+            'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundaryAgjAve4UZAXpjupu',
+            'Cookie': NGA_cookie,
+            'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 NGA_skull/7.1.8',
+        },
+        body: "------WebKitFormBoundaryAgjAve4UZAXpjupu\r\nContent-Disposition: form-data; name=\"__lib\"\r\n\r\nmission\r\n------WebKitFormBoundaryAgjAve4UZAXpjupu\r\nContent-Disposition: form-data; name=\"__output\"\r\n\r\n11\r\n------WebKitFormBoundaryAgjAve4UZAXpjupu\r\nContent-Disposition: form-data; name=\"app_id\"\r\n\r\n1001\r\n------WebKitFormBoundaryAgjAve4UZAXpjupu\r\nContent-Disposition: form-data; name=\"__act\"\r\n\r\ncheckin_count_add\r\n------WebKitFormBoundaryAgjAve4UZAXpjupu\r\nContent-Disposition: form-data; name=\"access_uid\"\r\n\r\n" + NGA_access_uid + "\r\n------WebKitFormBoundaryAgjAve4UZAXpjupu\r\nContent-Disposition: form-data; name=\"access_token\"\r\n\r\n" + NGA_access_token + "\r\n------WebKitFormBoundaryAgjAve4UZAXpjupu\r\nContent-Disposition: form-data; name=\"no_compatible_fix\"\r\n\r\n1\r\n------WebKitFormBoundaryAgjAve4UZAXpjupu\r\nContent-Disposition: form-data; name=\"mid\"\r\n\r\n131\r\n"
+    }
+
+    $gabeX.post(options, function (errors, response, body) {
+        console.log(JSON.parse(body))
+    });
 }
 
 
