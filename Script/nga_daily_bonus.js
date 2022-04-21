6f5f109d1360bc18ce22558e495a231661795d48
zhaoguibin <>
2022-04-21 13:49:00 +0800

    nga

--- Script/nga_daily_bonus.js
+++ Script/nga_daily_bonus.js
@@ -190,6 +190,99 @@ function decodeJson(errors, response, body) {
     addUpDailyBonus();
 }
 
+//每天分享帖子三次
+const sharePost = function () {
+    let share_options = {
+        url: "https://ngabbs.com/nuke.php?__lib=data_query&__act=topic_share_log_v2",
+        headers: {
+            'Host': 'ngabbs.com',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 NGA_skull/7.1.8',
+        },
+        body: "__output=12&access_token=" + NGA_access_token + "&access_uid=" + NGA_access_uid + "&app_id=1001&event=2&tid=31470825"
+    }
+
+    return new Promise(function (resolve, reject) {
+        gabe.post(share_options, function (errors, response, body) {
+            resolve('nga分享帖子');
+        });
+    });
+}
+
+//分享帖子之后获取N币
+const sharePostGetCoin = function () {
+    let body = {
+        __lib: "mission",
+        __output: "11",
+        app_id: "1001",
+        __act: "check_mission",
+        mid: "149",
+        get_success_repeat: "1",
+        no_compatible_fix: "1",
+        access_uid: NGA_access_uid,
+        access_token: NGA_access_token,
+        __inchst: "UTF-8"
+    };
+
+    options.body = bodyData(body);
+
+    return new Promise(function (resolve, reject) {
+        gabe.post(options, function (errors, response, body) {
+            let msg = '';
+            let data = JSON.parse(body);
+            if (data.data[0][2][149]) {
+                msg = data.data[0][2][149];
+            } else {
+                msg = '分享帖子获取N币失败';
+            }
+            resolve(msg);
+        });
+
+    });
+}
+
+//看广告获取N币
+const viewAdGetCoin = function () {
+    let body = {
+        __lib: "mission",
+        __output: "11",
+        app_id: "1001",
+        __act: "video_view_task_counter_add_v2",
+        get_success_repeat: "1",
+        no_compatible_fix: "1",
+        access_uid: NGA_access_uid,
+        access_token: NGA_access_token,
+        __inchst: "UTF-8"
+    };
+
+    options.body = bodyData(body);
+
+    return new Promise(function (resolve, reject) {
+        gabe.post(options, function (errors, response, body) {
+            resolve('观看广告获取N币');
+        });
+    });
+}
+
+//分享帖子&&观看广告
+async function sharePostAndViewAd() {
+    //分享帖子
+    const share_first = await sharePost();
+    const share_second = await sharePost();
+    const share_third = await sharePost();
+    const share_get_coin = await sharePostGetCoin();
+    //查看广告
+    const view_ad_first = await viewAdGetCoin();
+    const view_ad_second = await viewAdGetCoin();
+    const view_ad_third = await viewAdGetCoin();
+    const view_ad_fourth = await viewAdGetCoin();
+
+    let msg = '第一次分享：' + share_first + "\r\n" + '第二次分享：' + share_second + "\r\n" + '第三次分享：' + share_third + "\r\n" + '分享结果：' + share_get_coin + "\r\n"
+        + "第一次看广告：" + view_ad_first + "\r\n" + "第二次看广告：" + view_ad_second + "\r\n" + "第三次看广告：" + view_ad_third + "\r\n" + "第四次看广告：" + view_ad_fourth + "\r\n";
+    gabe.notify('nga分享帖子&&观看广告', '', msg);
+}
+
 checkIn();
+sharePostAndViewAd();
 
 $done();
