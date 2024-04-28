3d632095103b140c00642d5323a4bbb5a68318c5
ZhaoGuibin <940508445@qq.com>
2024-04-28 09:24:39 +0800

    Update hifini_sign.js

--- Script/hifini_sign.js
+++ Script/hifini_sign.js
@@ -2,7 +2,7 @@
  [Script]
  hifini签到.js = type=cron,cronexp=35 8 * * *,wake-system=1,timeout=20,script-path=https://raw.githubusercontent.com/zhaoguibin/surge/master/Script/hifini_sign.js,script-update-interval=0
 
- 获取hifini的cookie = type=http-request,pattern=https:\/\/www\.hifini\.com\/sg_sign\.htm,tag=获取hifini的cookie,script-path=https://raw.githubusercontent.com/zhaoguibin/surge/master/Script/hifini_sign.js,script-update-interval=0
+ 获取hifini的cookie = type=http-response,pattern=https:\/\/www\.hifini\.com\/sg_sign\.htm,tag=获取hifini的cookie,script-path=https://raw.githubusercontent.com/zhaoguibin/surge/master/Script/hifini_sign.js,script-update-interval=0
  个人页面获取到cookie，https://www.hifini.com
 
  [MITM]
@@ -10,17 +10,8 @@
  *************************/
 
 const isRequest = typeof $request != "undefined"
-// if (isRequest) {
-//     let body = $request.body;
-//  $notification.post('HIFINI签到', '', body);
-//  console.log(body);
-//  rep_body = $response.body;
-//  console.log(rep_body);
-// }
-
-const isResponse = typeof $response != "undefined"
-if(isResponse){
+if (isRequest) {
  rep_body = $response.body;
  console.log(rep_body);
+ $done({});
 }
-$done({});
