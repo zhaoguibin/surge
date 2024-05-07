f3f3941f37292cc49d030beccce834f0617adac2
ZhaoGuibin <940508445@qq.com>
2024-05-07 09:53:24 +0800

    Update hifini_sign.js

--- Script/hifini_sign.js
+++ Script/hifini_sign.js
@@ -3,7 +3,7 @@
  hifini签到.js = type=cron,cronexp=35 8 * * *,wake-system=1,timeout=20,script-path=https://raw.githubusercontent.com/zhaoguibin/surge/master/Script/hifini_sign.js,script-update-interval=0
 
  获取hifini的cookie = type=http-request,pattern=https:\/\/www\.hifini\.com,tag=获取hifini的sign,script-path=https://raw.githubusercontent.com/zhaoguibin/surge/master/Script/hifini_sign.js
- 个人页面获取到cookie，https://www.hifini.com
+获取cookie，https://www.hifini.com
 
  [MITM]
  hostname = *hifini.com*
