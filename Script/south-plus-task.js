730af2b1e25d9df95f5e36c554a63d4b119d38b6
ZhaoGuibin <940508445@qq.com>
2025-06-22 09:53:31 +0800

    Update south-plus-task.js

--- Script/south-plus-task.js
+++ Script/south-plus-task.js
@@ -18,7 +18,7 @@ if (isRequest) {
         $done({});
     }
 
-     let user_agent = $request.headers['User-Agent'];
+     let user_agent = $request.headers['user-Agent'];
     if (!user_agent) {
         $notification.post('', '', '获取[south-plus]user_agent失败');
         $done({});
